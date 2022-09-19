b2d7482eea04afcee902c3110202d6f38330d7ac
Mateus Bazzei <mateus.bazzei@alunos.sc.senac.br>
2022-09-19 19:38:17 -0300

    composicao carro

---
+++ composicao/motor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicao;
+
+/**
+ *
+ * @author mateus.bazzei
+ */
+public class motor {
+    private int potencia;
+
+    /**
+     * @return the potencia
+     */
+    public int getPotencia() {
+        return potencia;
+    }
+
+    /**
+     * @param potencia the potencia to set
+     */
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+}
